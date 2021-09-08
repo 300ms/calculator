c41a86faedd45d96079c4e03841e46d4a9128745
300ms <ilhansnmz44@gmail.com>
2021-09-08 18:46:49 +0300

    Fix linter errors and remove comments

--- src/components/App.js
+++ src/components/App.js
@@ -3,35 +3,6 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../Logic/calculate';
 
-// export default class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       total: null,
-//       next: null,
-//       operation: null,
-//     };
-
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick(buttonName) {
-//     const newState = Calculate(this.state, buttonName);
-//     this.setState(newState);
-//   }
-
-//   render() {
-//     const { total, next } = this.state;
-//     return (
-//       <>
-//         <div className="display-container"><Display value={next || total || '0'} /></div>
-//         <div className="button-panel-container"><ButtonPanel onClick={this.handleClick} /></div>
-//       </>
-//     );
-//   }
-// }
-
 const App = () => {
   const [total, setTotal] = useState(null);
   const [next, setNext] = useState(null);
@@ -42,9 +13,6 @@ const App = () => {
     setTotal(newState.total);
     setNext(newState.next);
     setOperation(newState.operation);
-    // console.log(`total : ${total}`);
-    // console.log(`next : ${next}`);
-    // console.log(`operation : ${operation}`);
   };
 
   return (
